7f4c4f7247a27ba68396f27ab56ad0196b114cb0
luckyxp <2271613696@qq.com>
2020-02-17 18:59:45 +0800

    new

---
+++ src/main/java/com/xp/pojo/User.java
@@ -0,0 +1,14 @@
+package com.xp.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private String sex;
+}
